40ab15b806e8bcb8f3b4d19b0d48ccd886fe433d
Moshiur Rahman <unimrgm@gmail.com>
2018-01-24 21:42:43 +0600

    Header redesigned

---
+++ royalehome/js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    var $product = $(".rooms");
+    $product.on('mouseover', function () {
+        glider($product);
+    }).slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        useCSS: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    var $quotes = $(".quotes");
+    $quotes.slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        pauseOnHover: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // Mouseover slide only
+    // var $hero = $(".hero");
+    // $hero.slick({
+    //     dots: false,
+    //     infinite: true,
+    //     arrows: false,
+    //     speed: 1000,
+    //     autoplay: true,
+    //     autoplaySpeed: 4500,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     fade: true,
+    //     pauseOnHover: true,
+    //     useCSS: true,
+    //     useTransform: true,
+    //     cssEase: 'ease-in-out'
+    // });
+
+    // Mouseover slide only
+    function glider($slide) {
+        $slide.on('wheel', {
+            $slide: $slide
+        }, function (event) {
+            event.preventDefault();
+            var $slide = event.data.$slide;
+            var delta = event.originalEvent.deltaY;
+            if (delta > 0) {
+                $slide.slick('slickPrev');
+            } else {
+                $slide.slick('slickNext');
+            }
+        });
+    }
+});
